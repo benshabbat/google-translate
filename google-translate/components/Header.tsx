0ce12d76243b0ea4156f22b2f80469ad68d918d3
benshabbat <benshabbat27@gmail.com>
2024-06-19 20:45:27 +0300

    add img logo

--- google-translate/components/Header.tsx
+++ google-translate/components/Header.tsx
@@ -1,12 +1,22 @@
 import { auth } from "@clerk/nextjs/server";
+import Image from "next/image";
 import Link from "next/link";
-
+import logo from "./Google_Translate_(4).png"
 const Header = () => {
   const { userId } = auth();
   return (
     <header>
       <div>
-        <Link href=""></Link>
+        <Link href="/">
+            <Image
+            src={logo}
+            alt="logo"
+            width={200}
+            height={100}
+            className="object-contain h-32 cursor-pointer">
+
+            </Image>
+        </Link>
       </div>
     </header>
   );
