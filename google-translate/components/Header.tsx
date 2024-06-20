6dffdc55e4748830b29cdeeaa815a8c5b7ee7dbe
benshabbat <benshabbat27@gmail.com>
2024-06-20 22:20:19 +0300

    css for header

--- google-translate/components/Header.tsx
+++ google-translate/components/Header.tsx
@@ -1,39 +1,29 @@
 import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
 import Link from "next/link";
-import logo from "./Google_Translate_(4).png"
-import {
-    ClerkProvider,
-    SignInButton,
-    SignedIn,
-    SignedOut,
-    UserButton,
-  } from "@clerk/nextjs";
+import logo from "./Google_Translate_(4).png";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 const Header = () => {
   const { userId } = auth();
   return (
     <header className="flex items-center justify-between px-8 border-b mb-5">
       <div>
         <Link href="/">
-            <Image
+          <Image
             src={logo}
             alt="logo"
             width={200}
             height={100}
-            className="object-contain h-32 cursor-pointer">
-
-            </Image>
+            className="object-contain h-32 cursor-pointer"
+          ></Image>
         </Link>
-        
       </div>
-      <div>
       <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-      </div>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </header>
   );
 };
