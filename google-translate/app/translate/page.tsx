342ca13b5670015d731f7166181cbc4cdfd4579c
benshabbat <benshabbat27@gmail.com>
2024-06-21 10:56:58 +0300

    languageEndpoint

--- google-translate/app/translate/page.tsx
+++ google-translate/app/translate/page.tsx
@@ -4,7 +4,7 @@ import { auth } from "@clerk/nextjs/server";
 const TranslatePage = () => {
   auth().protect();
   const { userId } = auth();
- 
+  const languageEndpoint = "https://api.cognitive.microsofttranslator.com/languages?api-version=3.0"
   return (
     <div>
       <TranslationForm />
