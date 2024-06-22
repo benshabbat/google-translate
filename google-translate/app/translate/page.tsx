b702db21aa57aba8108bf9ff5b8f3d67bb666ab2
benshabbat <benshabbat27@gmail.com>
2024-06-23 00:02:06 +0300

    TranslationLanguages

--- google-translate/app/translate/page.tsx
+++ google-translate/app/translate/page.tsx
@@ -1,15 +1,26 @@
 import TranslationForm from "@/components/TranslationForm";
 import { auth } from "@clerk/nextjs/server";
 
-const TranslatePage = async() => {
+export type TranslationLanguages = {
+  translation: {
+    [key: string]: {
+      name: string;
+      nativeName: string;
+      dir: "ltr" | "rtl";
+    };
+  };
+};
+const TranslatePage = async () => {
   auth().protect();
   const { userId } = auth();
-  const languageEndpoint = "https://api.cognitive.microsofttranslator.com/languages?api-version=3.0"
-  const response = await fetch(languageEndpoint,{
-    next:{
-        revalidate:60*60*24//cache the res 24h refresh
-    }
-  })
+  const languageEndpoint =
+    "https://api.cognitive.microsofttranslator.com/languages?api-version=3.0";
+  const response = await fetch(languageEndpoint, {
+    next: {
+      revalidate: 60 * 60 * 24, //cache the res 24h refresh
+    },
+  });
+  
   return (
     <div>
       <TranslationForm />
