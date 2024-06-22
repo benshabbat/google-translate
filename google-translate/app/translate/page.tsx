127a73e02de8dc60514beb0919cb6d6480758bef
benshabbat <benshabbat27@gmail.com>
2024-06-22 23:57:13 +0300

    revalidate

--- google-translate/app/translate/page.tsx
+++ google-translate/app/translate/page.tsx
@@ -1,10 +1,15 @@
 import TranslationForm from "@/components/TranslationForm";
 import { auth } from "@clerk/nextjs/server";
 
-const TranslatePage = () => {
+const TranslatePage = async() => {
   auth().protect();
   const { userId } = auth();
   const languageEndpoint = "https://api.cognitive.microsofttranslator.com/languages?api-version=3.0"
+  const response = await fetch(languageEndpoint,{
+    next:{
+        revalidate:60*60*24//cache the res 24h refresh
+    }
+  })
   return (
     <div>
       <TranslationForm />
